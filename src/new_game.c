dd78d60cf93eddf48453c427bda5f8a565de997e
topher235 <chris.hurt@mobelux.com>
2023-04-30 21:42:50 -0400

    exp share on all the time

--- src/new_game.c
+++ src/new_game.c
@@ -205,7 +205,7 @@ void NewGameInitData(void)
     ResetTrainerHillResults();
     ResetContestLinkResults();
     memset(&gSaveBlock2Ptr->follower, 0, sizeof(gSaveBlock2Ptr->follower));
-    gSaveBlock2Ptr->expShare = 0;
+    gSaveBlock2Ptr->expShare = 1;
 }
 
 static void ResetMiniGamesRecords(void)
