1e0907578f0b5015ba1ad97ce6d4cb1339414814
topher235 <chris.hurt@mobelux.com>
2023-05-24 20:15:37 -0400

    Adds letter UI and calls from Letter Item

---
+++ include/letter_ui.h
@@ -0,0 +1,10 @@
+#ifndef GUARD_LETTER_UI_H
+#define GUARD_LETTER_UI_H
+
+#include "main.h"
+
+// void Task_OpenUI(u8 taskId);
+void LetterUI_Init(MainCallback callback);
+
+
+#endif // GUARD_LETTER_UI_H
