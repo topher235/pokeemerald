563bc8523af55e3dc8322002fc15b5585dd6e1a5
topher235 <chris.hurt@mobelux.com>
2023-06-07 22:53:37 -0400

    Update decoded letters icons

--- src/strings.c
+++ src/strings.c
@@ -1828,5 +1828,6 @@ const u8 gText_ExpShareOff[] = _("Turned off the Exp. Share.\pParty will no long
 
 
 // Letters
+// maximum number of characters per line: ~41
 const u8 gText_LetterPageOne[] = _("Page 1\nLine 2\n Line3\n Line 4\nLine 5\nLine 6");
 const u8 gText_LetterPageTwo[] = _("Page 2\na really really really lo- a really reall");
