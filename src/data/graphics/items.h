ee4a70455de03a1edd94b6c7c6b2811c061e4ed1
topher235 <chris.hurt@mobelux.com>
2023-06-14 15:30:57 -0400

    Modifies PP UP icon to be redbull can

--- src/data/graphics/items.h
+++ src/data/graphics/items.h
@@ -736,3 +736,6 @@ const u32 gItemIconPalette_MagmaEmblem[] = INCBIN_U32("graphics/items/icon_palet
 
 const u32 gItemIcon_OldSeaMap[] = INCBIN_U32("graphics/items/icons/old_sea_map.4bpp.lz");
 const u32 gItemIconPalette_OldSeaMap[] = INCBIN_U32("graphics/items/icon_palettes/old_sea_map.gbapal.lz");
+
+const u32 gItemIcon_Redbull[] = INCBIN_U32("graphics/items/icons/redbull.4bpp.lz");
+const u32 gItemIconPalette_Redbull[] = INCBIN_U32("graphics/items/icon_palettes/redbull.gbapal.lz");
