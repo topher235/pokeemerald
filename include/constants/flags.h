51d7bb3db5e0b1f3b3dbf9294eda067e614490c4
topher235 <chris.hurt@mobelux.com>
2023-06-24 23:53:25 -0400

    Add npc dialogue plot clues

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -1241,7 +1241,7 @@
 #define FLAG_READ_LETTER_6                                          0x4A7
 #define FLAG_READ_LETTER_7                                          0x4A8
 #define FLAG_HIDE                                                   0x4A9
-#define FLAG_UNUSED_0x4AA                                           0x4AA // Unused Flag
+#define FLAG_RECEIVED_CALL_LETTER_1_DECODED                         0x4AA
 #define FLAG_UNUSED_0x4AB                                           0x4AB // Unused Flag
 #define FLAG_UNUSED_0x4AC                                           0x4AC // Unused Flag
 #define FLAG_UNUSED_0x4AD                                           0x4AD // Unused Flag
